7c70267c1d86c554998c41ae12d2203ae49fa277
Nithin S J <nithinjayanjasla@gmail.com>
2025-04-08 18:55:29 +0530

    cart page complete

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -45,7 +45,7 @@ const Cart = () => {
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
           Shopping Cart{" "}
-          <span className="text-sm text-indigo-500">{getCartCount()}Items</span>
+          <span className="text-sm text-primary">{getCartCount()} Items</span>
         </h1>
 
         <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -83,7 +83,7 @@ const Cart = () => {
                   </p>
                   <div className="flex items-center">
                     <p>Qty:</p>
-                    <select className="outline-none">
+                    <select onChange={e=> updateCartItem(product._id, Number(e.target.value))} value={cartItems[product._id]} className="outline-none">
                       {Array(
                         cartItems[product._id] > 9 ? cartItems[product._id] : 9
                       )
@@ -103,11 +103,11 @@ const Cart = () => {
               {product.offerPrice * product.quantity}
             </p>
             <button
-              onClick={() => removeFromCart()}
+              onClick={() => removeFromCart(product._id)}
               className="cursor-pointer mx-auto"
             >
               <img
-                src={assets.refresh_icon}
+                src={assets.remove_icon}
                 alt="remove"
                 className="inline-block w-6 h-6"
               />
@@ -120,7 +120,7 @@ const Cart = () => {
             navigate("/products");
             scrollTo(0, 0);
           }}
-          className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium"
+          className="group cursor-pointer flex items-center mt-8 gap-2 text-primary font-medium"
         >
           <img
             src={assets.arrow_right_icon_colored}
@@ -145,7 +145,7 @@ const Cart = () => {
             </p>
             <button
               onClick={() => setShowAddress(!showAddress)}
-              className="text-indigo-500 hover:underline cursor-pointer"
+              className="text-primary hover:underline cursor-pointer"
             >
               Change
             </button>
@@ -162,7 +162,7 @@ const Cart = () => {
                 ))}
                 <p
                   onClick={() =>navigate("/add-address")}
-                  className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10"
+                  className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10"
                 >
                   Add address
                 </p>
@@ -199,7 +199,7 @@ const Cart = () => {
           </p>
         </div>
 
-        <button onClick={placeOrder} className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition">
+        <button onClick={placeOrder} className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-primary-dull transition">
         {paymentOption === "COD" ? "Place Order" : "Proceed to Checkout"}
         </button>
       </div>
