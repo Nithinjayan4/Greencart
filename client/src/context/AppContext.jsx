98f483c8b3b4f0e8197f1cc1e7d81109ea721880
Nithin S J <nithinjayanjasla@gmail.com>
2025-04-08 16:19:14 +0530

    price issue

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -64,6 +64,29 @@ export const AppContextProvider = ({ children }) => {
     
     }
 
+    //get cat item count
+    const getCartCount = () => {
+        let totalCount=0;
+        for (const item in cartItems) {
+            totalCount += cartItems[item]
+        }
+        return totalCount
+    };
+
+    //get cart total  amount
+    const getCartAmount = () => {
+        let totalAmount = 0
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items)
+           if(cartItems[items] > 0){
+
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+           
+        }
+        return Math.floor(totalAmount * 100) / 100
+    }
+}
+
 
     useEffect(() => {
         fetchProducts()
@@ -85,6 +108,8 @@ export const AppContextProvider = ({ children }) => {
         cartItems,
         searchQuery,
         setSearchQuery,
+        getCartAmount,
+        getCartCount,
     };
 
 
