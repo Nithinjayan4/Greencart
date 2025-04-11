4f917d1e88128f986f5264d42281bc01fa1f142f
Nithin S J <nithinjayanjasla@gmail.com>
2025-04-11 12:31:21 +0530

    front end link added in backend server

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary()
 
 //Allow Multiple origin
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://greencart-seven.vercel.app"];
 
 app.post('/stripe',express.raw({ type: 'application/json' }),stripeWebhook);
 
